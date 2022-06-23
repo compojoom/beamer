90cd13930e45649590de47bbe2741ae63e2aada9
manuelwedler <manuel.wedler@googlemail.com>
2022-06-23 12:41:27 +0200

    frontend: Remove FormKit library
    we didn't see a benefit in using it. It was just complicated to use and the default vue value bindings for forms just work perfectly. The TextInput component is solely introduced to gather the styling of the input.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,37 +3,15 @@ import '@fontsource/sora';
 import 'vue-select/dist/vue-select.css';
 import 'floating-vue/dist/style.css';
 
-import {
-  createInput,
-  defaultConfig as formkitConfig,
-  plugin as formkitPlugin,
-} from '@formkit/vue';
 import FloatingVue from 'floating-vue';
 import { createPinia } from 'pinia';
 import piniaPersistState from 'pinia-plugin-persistedstate';
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import Selector from './components/inputs/Selector.vue';
-import formkitTheme from './formkitTheme';
 import router from './router';
 
 const pinia = createPinia();
 pinia.use(piniaPersistState);
 
-createApp(App)
-  .use(router)
-  .use(
-    formkitPlugin,
-    formkitConfig({
-      plugins: [formkitTheme],
-      inputs: {
-        selector: createInput(Selector, {
-          props: ['options'],
-        }),
-      },
-    }),
-  )
-  .use(FloatingVue)
-  .use(pinia)
-  .mount('#app');
+createApp(App).use(router).use(FloatingVue).use(pinia).mount('#app');
